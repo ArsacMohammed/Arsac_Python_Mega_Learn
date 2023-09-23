2469da3fffb0730b7276d0429d7d57136ee9411b
ArsacMohammed <arsacskasha@gmail.com>
2023-09-23 16:44:46 +0530

    added error handling for the edit and complete feature

--- Python_Basics/main.py
+++ Python_Basics/main.py
@@ -3,8 +3,9 @@
 while True:
     user_actions= input ("Type add ,edit ,show ,complete or exit ").strip()
 
-
-    if "add" in  user_actions:
+# this program will produce an error if the input for the user_actions is edit add a new member , here the add will get executed
+# to avoid this we use startswith function.
+    if user_actions.startswith("add"):
         with open("todo.txt","r") as file:
             todos = file.readlines()
 
@@ -13,7 +14,7 @@ while True:
 
         with open("todo.txt", "w") as file:
             file.writelines(todos)
-    elif "show" in user_actions:
+    elif user_actions.startswith("show"):
         with open("todo.txt","r") as file:
             todos = file.readlines()
 
@@ -21,27 +22,35 @@ while True:
             todo=todo.strip("\n")
             todo = todo.title()
             print(f"{index + 1}-{todo}")
-    elif "edit" in user_actions :
-        number =int( user_actions[5:])
+    elif user_actions.startswith("edit") :
+        try:
+            number =int( user_actions[5:])
 
-        with open("todo.txt","r") as file:
-            todos = file.readlines()
-        edited_todo = input("enter the todo for the new todo: ") +"\n"
-        todos[number - 1] = edited_todo
-        with open("todo.txt","w") as file:
-            file.writelines(todos)
+            with open("todo.txt","r") as file:
+                todos = file.readlines()
+            edited_todo = input("enter the todo for the new todo: ") +"\n"
+            todos[number - 1] = edited_todo
+            with open("todo.txt","w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("your command is not valid ")
+            continue
+    elif user_actions.startswith("complete"):
+        try:
+            number = int (user_actions[9:])
+            with open("todo.txt","r") as file:
+                todos = file.readlines()
+            todos.pop(number -1 )
 
-    elif "complete"  in user_actions:
-        number = int (user_actions[9:])
-        with open("todo.txt","r") as file:
-            todos = file.readlines()
-        todos.pop(number -1 )
+            with open("todo.txt","w") as file:
+                file.writelines(todos)
+        except IndexError:
+            print("There is no element with that number .")
+            continue
+        except ValueError:
+            print("Your command is not valid ..")
 
-        with open("todo.txt","w") as file:
-            file.writelines(todos)
-
-
-    elif "exit" in user_actions:
+    elif user_actions.startswith("exit"):
         break
 
     else:
