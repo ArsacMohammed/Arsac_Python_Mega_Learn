c65dc55e1b1dd7d2d81f018fad2a49c18ef8480f
ArsacMohammed <arsacskasha@gmail.com>
2023-09-25 10:40:27 +0530

    Implement "add todo items" feature

--- Python_Basics/web.py
+++ Python_Basics/web.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import functions
+
 st.title("Todo App")
 st.subheader("This Todo App will increase your productivity")
-st.write ("Things to do ")
-
-st.checkbox("go to the market")
-st.checkbox("play fortnite")
-st.checkbox("play pubg")
-st.checkbox("get ferrari")
-st.checkbox("hit the gym")
+st.write("Things to do ")
+todos = functions.get_todo()
 
 
+def add_todo():
+    todo_to_add = st.session_state["new_todo"] +"\n"
+    todos.append(todo_to_add )
+    functions.write_todos(todos)
 
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Enter a todo ", placeholder="Add a todo ", on_change=add_todo, key="new_todo")
 print("hello")
