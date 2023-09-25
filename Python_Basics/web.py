ff7d44199db521a436c14c14a9435ff9ec5da81b
ArsacMohammed <arsacskasha@gmail.com>
2023-09-25 10:22:10 +0530

    create a static web interface

---
+++ Python_Basics/web.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import functions
+st.title("Todo App")
+st.subheader("This Todo App will increase your productivity")
+st.write ("Things to do ")
+
+st.checkbox("go to the market")
+st.checkbox("play fortnite")
+st.checkbox("play pubg")
+st.checkbox("get ferrari")
+st.checkbox("hit the gym")
+
+
+
+print("hello")
