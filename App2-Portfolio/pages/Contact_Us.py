8b87ef720baa076667cf28afc50096be3c4b7f1f
ArsacMohammed <arsacskasha@gmail.com>
2023-10-03 14:06:14 +0530

    complete send email.

--- App2-Portfolio/pages/Contact_Us.py
+++ App2-Portfolio/pages/Contact_Us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Contact Information ")
+
+with st.form(key="email_form"):
+    st.text_input("Your Email Address")
+    st.text_area("your Message here!!")
+    Button = st.form_submit_button("Submit")
+    if Button:
+        print("i am pressed")
