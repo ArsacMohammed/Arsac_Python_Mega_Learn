b0852dceedfd86cbeeb74e475b04fd7f1e45a9b0
ArsacMohammed <arsacskasha@gmail.com>
2023-10-03 14:44:29 +0530

    send email using the form

--- App2-Portfolio/send_email.py
+++ App2-Portfolio/send_email.py
@@ -1,15 +1,13 @@
 import smtplib,ssl
-username="kit.23.19bec068@gmail.com"
-password="jlps aojf llsp hqwb"
-host ="smtp.gmail.com"
-port=465
-context=ssl.create_default_context()
-receiver="kit.23.19bec068@gmail.com"
-message="""
-HI! 
-How Are you??
-Bye
-"""
-with smtplib.SMTP_SSL(host,port,context=context)as server:
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
+
+
+def send_email(message):
+    username="kit.23.19bec068@gmail.com"
+    password="jlps aojf llsp hqwb"
+    host ="smtp.gmail.com"
+    port=465
+    context=ssl.create_default_context()
+    receiver="kit.23.19bec068@gmail.com"
+    with smtplib.SMTP_SSL(host,port,context=context)as server:
+        server.login(username,password)
+        server.sendmail(username,receiver,message)
