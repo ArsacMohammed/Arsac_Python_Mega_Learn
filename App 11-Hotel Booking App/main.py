9f7128832f8f3664bd618d7ac4587fb5888fd576
ArsacMohammed <arsacskasha@gmail.com>
2023-10-04 22:11:19 +0530

    credit card and secure credit card feature added using inheritance.

--- App 11-Hotel Booking App/main.py
+++ App 11-Hotel Booking App/main.py
@@ -3,6 +3,12 @@ import pandas as pd
 df = pd.read_csv(r"C:\Users\mm\Arsac_Python_Mega_learn\Python\App 11-Hotel Booking App\data\hotels.csv",
                  dtype={"id": str})
 
+df_cards = pd.read_csv(r"C:\Users\mm\Arsac_Python_Mega_learn\Python\App 11-Hotel Booking App\data\cards.csv",
+                       dtype=str).to_dict(
+    orient="records")
+df_card_security = pd.read_csv(
+    r"C:\Users\mm\Arsac_Python_Mega_learn\Python\App 11-Hotel Booking App\data\card_security.csv", dtype=str)
+
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -36,13 +42,43 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, cvc, holder):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "cvc": cvc, "holder": holder}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_card_security.loc[df_card_security["number"] == self.number, "password"].squeeze()
+        if given_password == password:
+            return True
+        else:
+            return False
+
+
 print(df.head(5))
 hotel_id = input("Enter the id of the Hotel : ")
 hotel = Hotel(hotel_id)
+
 if hotel.available():
-    hotel.book()
-    customer_name = input("Enter your name")
-    reserve_ticket = ReservationTicket(customer_name, hotel)
-    print(reserve_ticket.generate())
+    credit_card = SecureCreditCard(number="1234")
+    if credit_card.validate(expiration="12/26", cvc="123", holder="JOHN SMITH"):
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            customer_name = input("Enter your name")
+            reserve_ticket = ReservationTicket(customer_name, hotel)
+            print(reserve_ticket.generate())
+        else:
+            print("Authentication failed !")
+    else:
+        print("There is something wrong with your card....Try again")
 else:
     print("Hotel is not free!!")
