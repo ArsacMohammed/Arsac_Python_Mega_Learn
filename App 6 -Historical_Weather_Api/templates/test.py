d7395a275db661c3dd1622922cbf2a38feed272d
ArsacMohammed <arsacskasha@gmail.com>
2023-10-04 18:00:18 +0530

    show datatable on the webpage and URL end point for all data and annual data - app completed

--- App 6 -Historical_Weather_Api/templates/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-
-filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
-df = pd.read_csv(filename, sep=",", skiprows=20, parse_dates=["    DATE"])
-print(df.head(5))
-print(filename)
